3b31f7873d6a54e2ab7f5ec341313a3f52170d88
Anahi1777 <castillogarciaanahi6@gmail.com>
2026-02-17 18:45:06 -0500

    Expresion.java

--- Expresiones.java
+++ Expresiones.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package triangulo;
 
-/**
- *
- * 
- */
 import java.util.Scanner;
 public class Expresiones {
     public static void main(String[] args) {
@@ -106,4 +98,5 @@ public class Expresiones {
     }
     
 }
+
 
