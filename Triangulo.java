deecf69b32eb460f511217cc5626532098c17f04
Anahi1777 <castillogarciaanahi6@gmail.com>
2026-02-17 18:48:15 -0500

    Triangulo.java

--- Triangulo.java
+++ Triangulo.java
@@ -1,22 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package triangulo;
 
-/**
- *
- * 
- */
-
-// importamos la herramienta scanner para leer datos del usuario
 import java.util.Scanner;
 
 public class Triangulo {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
         //Crea un objeto para leer lo que escribe el usuario
         // Scaner es el tipo de dato (como double, int)
@@ -38,12 +26,7 @@ public class Triangulo {
         System.out.println("S = " + S);
     }
 }
-       
-
     
 
-        // TODO code application logic here
-    
-    
 
 
