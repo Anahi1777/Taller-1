3d379b199431dc6a5aa89ec5a8edf7bdc4f466fb
Anahi1777 <castillogarciaanahi6@gmail.com>
2026-02-17 18:43:21 -0500

    Expresion1.java

--- Expresion1.java
+++ Expresion1.java
@@ -1,20 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package triangulo;
-
-/**
- *
- * 
- */
 public class Expresion1 {
      public static void main(String[] args) {
         double A = 2;
         double B = 5;
-        
-        // Expresión: 3 * A - 4 * B / A ^ 2
-        //Math.pow: elevar al cuadrado
         double resultado = 3 * A - 4 * B / Math.pow(A, 2);
         
         System.out.println("A = " + A);
@@ -23,4 +11,5 @@ public class Expresion1 {
     }
     
 }
+
 
