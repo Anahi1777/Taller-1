ba94e175f9f2339d976f6dd5f3c4eddbe194360a
Anahi1777 <castillogarciaanahi6@gmail.com>
2026-02-17 18:42:14 -0500

    Circulo.java

--- Circulo.java
+++ Circulo.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package triangulo;
 
-/**
- *
- * 
- */
 import java.util.Scanner;
 
   public class Circulo {
@@ -28,4 +21,5 @@ import java.util.Scanner;
     
     } 
 }
+
 
