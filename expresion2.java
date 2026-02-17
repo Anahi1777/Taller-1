07b3762e33367623d3eb7543f816ecd0b66f602b
Anahi1777 <castillogarciaanahi6@gmail.com>
2026-02-17 18:47:15 -0500

    expresion2.java

--- expresion2.java
+++ expresion2.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package triangulo;
 
 
@@ -12,4 +8,5 @@ public class expresion2 {
     }
     
 }
+
 
